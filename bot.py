892c94bcf2ad04be92430a9960e24a56ade6ca4a
Ivan-proger <ia_shkolin@mail.ru>
2020-11-25 16:57:04 +0300

    pred reliz

--- bot.py
+++ bot.py
@@ -1,32 +1,53 @@
-import telebot
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+
+from time import sleep
+
 import os
 
-bot = telebot.TeleBot('1476611133:AAHHvxmKTElEIaWNRYGVNWEg47TsDhf0jX8')
+app = Client("media")
 
-keyboard1 = telebot.types.ReplyKeyboardMarkup()
-keyboard1.row('Видео')
-print(keyboard1)
+@app.on_message(filters.command("type", prefixes=".") & filters.me)
+def type(_, msg):
+	orig_text = msg.text.split(".type ", maxsplit=1)[1]
+	text = orig_text
+	tbp = ""
+	typing_symbol = "}"
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-	bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
-
-@bot.message_handler(content_types=['text'])
-def send_text(message):
-	if message.text == 'Видео':
-		bot.send_message(message.chat.id, 'Привет, мой создатель')
-
+	while(tbp != orig_text):
 		try:
-			my_dir = os.getcwd()
-			j0in = os.path.join(my_dir, 'media')
-			os.chdir(j0in)
-		except FileNotFoundError:
-			print('так это 2 раз')
-		finally:
-			files = os.listdir(path=os.getcwd())
-			file = files[0]
-			video = open(file, 'rb')
-			bot.send_video(message.chat.id, video)
+			msg.edit(tbp + typing_symbol)
+			sleep(0.05)
+
+			tbp = tbp + text[0]
+			text = text[1:]
+
+			msg.edit(tbp)
+			sleep(0.05)
+
+		except FloodWait as e:
+			sleep(e.x)
+
+@app.on_message(filters.command("video", prefixes="."))
+def code1(app, msg):
+	text = "видево"
+	msg.edit_text("`" + text + "`")
+
+	try:
+		my_dir = os.getcwd()
+		j0in = os.path.join(my_dir, 'media')
+		os.chdir(j0in)
+	except FileNotFoundError:
+		print('так это 2 раз')
+	finally:
+		files = os.listdir(path=os.getcwd())
+		file = files[0]
+		video = open(file)
+		app.send_video("test_api_bott", file)
+
+#@app.on_message(filters.command("forw", prefixes="."))
+#def cod2(app, msg):
 
 
-bot.polling()
+
+app.run()
