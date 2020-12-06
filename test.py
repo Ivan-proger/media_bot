946e9fe9be41e62b584e9c85eccba5da1a0650cf
Ivan-proger <ia_shkolin@mail.ru>
2020-12-06 15:59:15 +0300

    heroku

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
-from pyrogram import Client, filters
+from pyrogram import Client
 
-app = Client(
-	'mood',
-	api_id=2297631,
-    api_hash="44c201618c3606df1a58012885311729"
-    )
+api_id = 2297631
+api_hash = "44c201618c3606df1a58012885311729"
+bot_token = "1476611133:AAHHvxmKTElEIaWNRYGVNWEg47TsDhf0jX8"
+info = "Greetings from **Heroku**!"
 
-@app.on_message(filters.me)
-def echo(client, message):
-	message.reply_text(message.text)
+app = Client(":memory:", api_id, api_hash, bot_token=bot_token)
+
+print(info)
+
+@app.on_message()
+def work(client, message):
+    app.send_message(message.chat.id, info)
 
 app.run()
