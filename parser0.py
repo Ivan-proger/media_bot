305307e056c5459c97f56f4b34311f6224b4b820
Ivan-proger <73116948+Ivan-proger@users.noreply.github.com>
2021-01-13 17:12:21 +0300

    the end

--- parser0.py
+++ parser0.py
@@ -5,8 +5,8 @@ import requests
 
 class parser_link():
 	def __init__(self):
-		self.DOMEN = 'https://www.xvideos.com'
-		self.url = ('https://www.xvideos.com/')
+		self.DOMEN = 'https://www.xxxxxxx.com'
+		self.url = ('https://www.xxxxxx.com/')
 		self.response = requests.get(self.url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0"})  #отправляем запрос на сайт для получения html 
 		self.soup = BeautifulSoup(self.response.text) #преобразуем html для нвшего soup
 		self.result_urls = ['sdsdsds']
